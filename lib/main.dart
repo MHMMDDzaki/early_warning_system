c1c31756dbf4dbc91d99c9efdaefaa996fed0f35
zetta-muhammad-dzaki <muhammad.dzaki@zettabyte.life>
2024-11-10 13:51:18 +0700

    alarm manager kiosk mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 import 'dart:async';
+import 'package:kiosk_mode/kiosk_mode.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';  // for json decoding
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +12,7 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget  {
+class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -21,16 +24,14 @@ stopAlarm() {
 
 startAlarm() {
   print('alarm start');
-  FlutterRingtonePlayer()
-      .play(fromAsset: "assets/duarr.mp3", looping: true,);
-
-  Timer(const Duration(seconds: 5), () {
-    stopAlarm();
-  });
+  FlutterRingtonePlayer().play(
+    fromAsset: "assets/duarr.mp3",
+    looping: true,
+  );
 }
 
-
 class _MyAppState extends State<MyApp> {
+  String _tempMessage = '';
 
   Future<void> scheduleAlarm() async {
     await AndroidAlarmManager.oneShot(
@@ -41,11 +42,6 @@ class _MyAppState extends State<MyApp> {
       exact: true,
       wakeup: true,
     );
-
-    // Automatically stop the alarm after 5 seconds
-    Timer(const Duration(seconds: 2), () {
-      scheduleCancelAlarm();
-    });
   }
 
   Future<void> scheduleCancelAlarm() async {
@@ -59,28 +55,79 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
+  Future<void> enterKioskMode() async {
+    try {
+      await startKioskMode();
+    } catch (e) {
+      print("Error entering kiosk mode: $e");
+    }
+  }
+
+  Future<void> exitKioskMode() async {
+    try {
+      await stopKioskMode();
+    } catch (e) {
+      print("Error exiting kiosk mode: $e");
+    }
+  }
+
+  Future<void> fetchData() async {
+    try {
+      // final response = await http.get(Uri.parse('http://192.168.0.124:3000/api/fine'));
+      final response = await http.get(Uri.parse('http://192.168.0.124:3000/api/trouble'));
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        double number = double.parse(data['message']);
+        if(number < 350) {
+          setState(() {
+            _tempMessage = 'aman';
+          });
+        } else {
+          setState(() {
+            _tempMessage = 'ga aman';
+          });
+          await enterKioskMode();
+          await scheduleAlarm();
+        }
+
+      } else {
+        print('Failed to load fine data');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Android alarm manager plus example'),
+          title: const Text('Alarm Manager with API'),
           elevation: 4,
           actions: [
             IconButton(
-              onPressed: () {
-                scheduleCancelAlarm();
+              onPressed: () async {
+                await exitKioskMode();
+                await scheduleCancelAlarm();
               },
               icon: const Icon(Icons.stop),
             ),
           ],
         ),
         body: Center(
-          child: OutlinedButton(
-            child: Text('Alarm now'),
-            onPressed: () async {
-              await scheduleAlarm();
-            },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                child: Text('Fetch Fine Data and Alarm'),
+                onPressed: () async {
+                  await fetchData();  // Fetch data from API
+                },
+              ),
+              SizedBox(height: 10),
+              Text('Fine Message: $_tempMessage'),
+            ],
           ),
         ),
       ),
